2eccd362cd7ddd86a26c719add258636f1fb067d
Y-H-Joe <237124812@qq.com>
2022-02-09 17:56:11 -0600

    update

--- EC_discriminator/source_code/ReactionTemplateGeneration/src/interfaces/IHelper.java
+++
@@ -1,8 +0,0 @@
-package interfaces;
-
-public interface IHelper {
-
-	/**Print a help page for the database extraction tool.
-	 */
-	public static void printHelp() {}
-}
--- network/transfer_prot_encoder_huggingface_20220208.py
+++ network/transfer_prot_encoder_huggingface_20220208.py
@@ -22,11 +22,79 @@ Contact: yihangjoe@foxmail.com
 
 ####=======================================================================####
 """
+"""
 from transformers import BertForMaskedLM, BertTokenizer, pipeline
 tokenizer = BertTokenizer.from_pretrained('Rostlab/prot_bert_bfd', do_lower_case=False )
 model = BertForMaskedLM.from_pretrained("Rostlab/prot_bert_bfd")
 unmasker = pipeline('fill-mask', model=model, tokenizer=tokenizer)
 unmasker('D L I P T S S K L V V [MASK] D T S L Q V K K A F F A L V T')
+"""
+
+import torch
+from torch import nn
+import re
+from transformers import BertForMaskedLM, BertTokenizer
+from transformers import pipeline
+
+## get model
+model = BertForMaskedLM.from_pretrained('./trained_models/')
+tokenizer = BertTokenizer.from_pretrained('./trained_models')
+## model to GPU
+device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+model = model.to(device)
+model.eval()
+
+## try
+seq = ' '.join([re.sub(r"[UZOB]", "X", sequence) for sequence in list('MIFDGKVAIITGGGKAKSIGYGIAVAYAK')])
+indexed_tokens = tokenizer.encode(seq)
+tokens_tensor = torch.tensor([indexed_tokens])
+## get prot sequences
+sequences_Example = ["A E T C Z A O","S K T Z P"]
+sequences_Example = [re.sub(r"[UZOB]", "X", sequence) for sequence in sequences_Example]
+
+## Tokenize, encode sequences and load it into the GPU if possibile
+ids = tokenizer.batch_encode_plus(sequences_Example, add_special_tokens=True, padding=True)
+input_ids = torch.tensor(ids['input_ids']).to(device)
+attention_mask = torch.tensor(ids['attention_mask']).to(device)
+
+##  Extracting sequences' features and load it into the CPU if needed
+with torch.no_grad():
+    embedding = model(input_ids=input_ids,attention_mask=attention_mask)[0]
+
+## Remove padding ([PAD]) and special tokens ([CLS],[SEP]) that is added by ProtBert-BFD model
+features = [] 
+for seq_num in range(len(embedding)):
+    # remove padding
+    seq_len = (attention_mask[seq_num] == 1).sum()
+    # remove special tokens
+    seq_emd = embedding[seq_num][1:seq_len-1]
+    features.append(seq_emd)
+
+
+
+
+
+
+
+## trash bin
+if False:
+    ## embeddings
+    fe = pipeline('feature-extraction', model=model, tokenizer=tokenizer,device=0 )
+    embedding = fe([seq])
+    embedding = torch.tensor(embedding)
+    
+    ## see model parameters in dict
+    model_dict = model.state_dict()
+    ## get encoder part of model
+    """
+    for name,_ in model.named_children():
+        print(name)
+    
+    bert
+    cls
+    """
+    model_bert = list(model.children())[0]
+    model_bert_dict = model_bert.state_dict()
 
 
 
